9b8248b540bd84bbb86abebd309db5e820ba7a4c
Jorge Montejo <56157795+MontejoJorge@users.noreply.github.com>
2021-04-17 19:29:38 +0200

    w2gRoom function moved to helpers

--- commands/w2g.js
+++ commands/w2g.js
@@ -1,27 +1,4 @@
-const fetch = require('node-fetch');
-
-const w2gApi = process.env.W2G_API_KEY;
-
-function w2gRoom(message, video) {
-    fetch("https://w2g.tv/rooms/create.json", {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "w2g_api_key": w2gApi,
-            "share": video,
-            "bg_color": "#404040",
-            "bg_opacity": "100"
-        })
-    })
-        .then(response => response.json())
-        .then(function (data) {
-            message.delete();
-            message.channel.send(`W2G: Here is your room, ${message.author}! \n https://w2g.tv/rooms/${data.streamkey}`);
-        });
-}
+const { w2gRoom } = require("../helpers/w2gRoom");
 
 module.exports = {
     name: 'w2g',
@@ -29,6 +6,8 @@ module.exports = {
     usage: "[video link]",
     execute(message, args) {
 
-        w2gRoom(message, args[0]);
+        const videoLink = args[0].toString();
+
+        w2gRoom(message, videoLink);
     },
 };
