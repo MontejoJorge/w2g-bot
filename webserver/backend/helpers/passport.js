464aa12056c8f6e7c92590fa6ad7d7cc55728cd3
Jorge Montejo <56157795+MontejoJorge@users.noreply.github.com>
2021-08-04 16:43:24 +0200

    feat: user model

--- webserver/backend/helpers/passport.js
+++ webserver/backend/helpers/passport.js
@@ -1,6 +1,8 @@
 const passport = require('passport');
 var Strategy = require('passport-discord').Strategy;
 
+const User = require("../models/user")
+
 passport.serializeUser(function (user, done) {
     done(null, user);
 });
@@ -17,10 +19,28 @@ passport.use(new Strategy({
     callbackURL: 'http://localhost:3000/api/auth/discord/callback',
     scope: scopes,
     prompt: prompt
-}, function(accessToken, refreshToken, profile, done) {
-    process.nextTick(function() {
+}, async (accessToken, refreshToken, profile, done) => {
+
+    const user = await User.findOne({ discordId: profile.id, });
+
+    if (!user) {
+
+        User.create({
+            username: profile.username,
+            email: profile.email,
+            avatar: profile.avatar,
+            discordDiscriminator: profile.discriminator,
+            discordId: profile.id
+            
+        }, function (err, profile) {
+            return done(err, profile);
+        });
+
+    } else {
         return done(null, profile);
-    });
+    }
+
+
 }));
 
 module.exports = passport;
---
+++ webserver/backend/models/user.js
@@ -0,0 +1,50 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    avatar: {
+        type: String
+    },
+    role: {
+        type: String,
+        default: "user",
+        required: true
+    },
+    enabled: {
+        type: Boolean,
+        default: true
+    },
+    discordId: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    discordDiscriminator: {
+        type: Number,
+        required: true
+    },
+    createdAt: Number,
+    updatedAt: Number,
+}, {
+    timestamps: {
+        currentTime: () => Math.floor(Date.now() / 1000)
+    }
+});
+
+UserSchema.methods.toJSON = function () {
+    const { __v, _id, ...user } = this.toObject();
+    user.uid = _id;
+
+    return user;
+}
+
+module.exports = model("User", UserSchema);
