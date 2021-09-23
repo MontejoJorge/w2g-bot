eff0cb69b661a668cb136b8a513831abff5a9b15
Jorge Montejo <jorgemon.lopez@gmail.com>
2021-09-23 10:39:10 +0200

    chore: dev and prod enviroments ⚙

--- app.js
+++ app.js
@@ -1,1 +1,2 @@
+require("./helpers/dev-prod");
 require("./bot");
--- bot.js
+++ bot.js
@@ -1,13 +1,61 @@
-require('dotenv').config()
 const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { Client, Collection, Intents } = require('discord.js');
+const AutoPoster = require('topgg-autoposter');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ 
+	intents: [
+		Intents.FLAGS.GUILDS, 
+		Intents.FLAGS.GUILD_MEMBERS,
+		Intents.FLAGS.GUILD_BANS,
+		Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+		Intents.FLAGS.GUILD_INTEGRATIONS,
+		Intents.FLAGS.GUILD_WEBHOOKS,
+		Intents.FLAGS.GUILD_INVITES,
+		Intents.FLAGS.GUILD_VOICE_STATES,
+		Intents.FLAGS.GUILD_PRESENCES,
+		Intents.FLAGS.GUILD_MESSAGES,
+		Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+		Intents.FLAGS.GUILD_MESSAGE_TYPING,
+		Intents.FLAGS.DIRECT_MESSAGES,
+		Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+		Intents.FLAGS.DIRECT_MESSAGE_TYPING
+	] 
+});
 client.commands = new Collection();
 
-const { DISCORD_TOKEN, DISCORD_CLIENT_ID, TOP_GG_TOKEN } = process.env;
+//Search commands
+const commands = [];
+const commandFolders = fs.readdirSync('./commands');
+
+for (const folder of commandFolders) {
+
+    const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+
+    for (const file of commandFiles) {
+        const command = require(`./commands/${folder}/${file}`);
+        commands.push(command.data.toJSON());
+		client.commands.set(command.data.name, command);
+    }
+}
+
+//Register slash commands
+const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN);
+
+(async () => {
+	try {
+		console.log('Started refreshing application (/) commands.');
+		
+		await rest.put(Routes.applicationCommands(process.env.DISCORD_CLIENT_ID), {
+			body: commands
+		});
+		
+		console.log('Successfully reloaded application (/) commands.');
+	} catch (error) {
+		console.error(error);
+	}
+})();
 
 //Search events
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
@@ -21,38 +69,11 @@ for (const file of eventFiles) {
 	}
 }
 
-//Search commands
-const commands = [];
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	commands.push(command.data.toJSON());
+if (process.env.NODE_ENV === "production") {
+    const ap = AutoPoster( process.env.TOP_GG_TOKEN, client);
+    ap.on('posted', () => { console.log('Posted stats to top.gg') });
 }
 
-const rest = new REST({ version: '9' }).setToken(token);
+client.login(process.env.DISCORD_TOKEN);
 
-(async () => {
-	try {
-		console.log('Started refreshing application (/) commands.');
-
-        await rest.put(
-            Routes.applicationCommands(DISCORD_CLIENT_ID),
-            { body: commands },
-        );
-
-		console.log('Successfully reloaded application (/) commands.');
-	} catch (error) {
-		console.error(error);
-	}
-})();
-
-const ap = AutoPoster(TOP_GG_TOKEN, client);
-
-ap.on('posted', () => { console.log('Posted stats to top.gg') });
-
-client.login(DISCORD_TOKEN);
-
-module.exports = {
-    client
-}
+module.exports = client;
