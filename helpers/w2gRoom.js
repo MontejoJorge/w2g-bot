e92434ecf90f75179b81126b64f6ec7651311c4f
Jorge Montejo <jorgemon.lopez@gmail.com>
2021-09-23 15:46:24 +0200

    refactor: w2g command to v13 🔨

--- helpers/w2gRoom.js
+++ helpers/w2gRoom.js
@@ -1,25 +1,22 @@
 const fetch = require('node-fetch');
 
-const { W2G_API_KEY } = process.env;
-
-function w2gRoom(message, videoLink) {
-    fetch("https://w2g.tv/rooms/create.json", {
+async function w2gRoom(videoLink = "") {
+    const res = await fetch("https://w2g.tv/rooms/create.json", {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            "w2g_api_key": W2G_API_KEY,
+            "w2g_api_key": process.env.W2G_API_KEY,
             "share": videoLink,
             "bg_color": "#404040",
             "bg_opacity": "100"
         })
     })
-        .then(response => response.json())
-        .then(function (data) {
-            message.channel.send(`W2G: Here is your room, ${message.author}! \n https://w2g.tv/rooms/${data.streamkey}`);
-        });
+    .then(response => { return response.json() });
+
+    return `https://w2g.tv/rooms/${res.streamkey}`
 }
 
 module.exports = {
