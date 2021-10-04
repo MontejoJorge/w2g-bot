7602773a9d81e244ec5420f32d7ba71aa8a066dd
Jorge Montejo <jorgemon.lopez@gmail.com>
2021-10-04 16:48:40 +0200

    feat: announcement pop up ✨

--- webserver/public/js/announcement.js
+++ webserver/public/js/announcement.js
@@ -1,10 +1,15 @@
 function validForm () {
-    let announcementTextarea = $("[name=announcementText]");
+    let announcementTextarea = $("[name=announcementText]").clone();
 
     if (announcementTextarea.val().length > 2000 || announcementTextarea.val().length == 0) {
         announcementTextarea.addClass("is-invalid");   
 
     } else {
+        $("#announcementModal").modal('toggle');
+        $("#submitAnnouncement").prop("disabled", true);
+        $("#submitAnnouncement span").removeClass("d-none");
+        announcementTextarea.prop("disabled", true);
+
         $("#announcementForm").submit();
     }
 }
