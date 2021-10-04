f7c32509900a6f20334b3efafd9cd9a472260029
Jorge Montejo <jorgemon.lopez@gmail.com>
2021-10-04 17:55:08 +0200

    fix: announcement msg 🐛

--- webserver/public/js/announcement.js
+++ webserver/public/js/announcement.js
@@ -2,13 +2,14 @@ function validForm () {
     let announcementTextarea = $("[name=announcementText]").clone();
 
     if (announcementTextarea.val().length > 2000 || announcementTextarea.val().length == 0) {
-        announcementTextarea.addClass("is-invalid");   
+        $("[name=announcementText]").addClass("is-invalid");
+        $("#announcementModal").modal('toggle');  
 
     } else {
         $("#announcementModal").modal('toggle');
         $("#submitAnnouncement").prop("disabled", true);
         $("#submitAnnouncement span").removeClass("d-none");
-        announcementTextarea.prop("disabled", true);
+        $("[name=announcementText]").prop("disabled", true);
 
         $("#announcementForm").submit();
     }
