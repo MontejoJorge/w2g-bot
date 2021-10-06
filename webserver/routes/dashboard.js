86c7966b5632d2dd44310017455fd71241089da7
Jorge Montejo <jorgemon.lopez@gmail.com>
2021-10-06 12:53:17 +0200

    style: dashboard 🎨

--- webserver/routes/dashboard.js
+++ webserver/routes/dashboard.js
@@ -5,6 +5,10 @@ const router = require("express").Router();
 
 router.use("/", auth);
 
+router.get("/", function(req, res) {
+    return res.render("dashboard");
+})
+
 router.get("/suggestion", suggestionGet);
 
 module.exports = router;
