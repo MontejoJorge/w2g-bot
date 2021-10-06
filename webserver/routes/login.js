212ad5986d2fb244c98da5708ad9fc20ca6120f0
MontejoJorge <jorgemon.lopez@gmail.com>
2021-10-06 20:06:12 +0200

    chore: https protocol ⚙

--- webserver/routes/login.js
+++ webserver/routes/login.js
@@ -4,7 +4,7 @@ router.get("/", function(req, res) {
     const oauth2 = new URL("https://discord.com/api/oauth2/authorize");
 
     oauth2.searchParams.append("client_id", process.env.DISCORD_CLIENT_ID);
-    oauth2.searchParams.append("redirect_uri", req.protocol + '://' + req.get('host') + "/auth/callback");
+    oauth2.searchParams.append("redirect_uri", 'https://' + req.get('host') + "/auth/callback");
     oauth2.searchParams.append("response_type", "code");
     oauth2.searchParams.append("scope", "identify email");
 
